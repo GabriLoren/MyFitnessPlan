51a21f4c9517813746ed4957669b66cb41ae3a6e
gabriloren <gabriel.lg90@gmail.com>
2023-06-03 21:18:35 +0200

    Creación Clase alimento

---
+++ src/modelo/Alimento.java
@@ -0,0 +1,25 @@
+package modelo;
+
+public class Alimento {
+	
+	private String nombre;
+	private int calorias;
+	private int proteinas;
+	private int carbohidratos;
+	private int grasas;
+	
+	
+	public Alimento(String nombre, int calorias, int proteinas, int carbohidratos, int grasas) {
+		super();
+		this.nombre = nombre;
+		this.calorias = calorias;
+		this.proteinas = proteinas;
+		this.carbohidratos = carbohidratos;
+		this.grasas = grasas;
+	}
+	
+	
+	
+	
+
+}
